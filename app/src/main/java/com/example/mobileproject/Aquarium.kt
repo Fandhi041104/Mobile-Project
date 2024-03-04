b075b2a4e31dc31ab68c72648f802d57aeadf662
fandhi <fandhirishaleh@gmail.com>
2024-03-05 04:46:30 +0700

    Tugas Codelab Aquarium

---
+++ app/src/main/java/com/example/mobileproject/Aquarium.kt
@@ -0,0 +1,52 @@
+package com.example.mobileproject
+
+import android.health.connect.datatypes.units.Temperature
+import java.util.*
+
+fun randomDay() : String {
+    val week = arrayOf ("Monday", "Tuesday", "Wednesday", "Thursday",
+        "Friday", "Saturday", "Sunday")
+    return week[Random().nextInt(week.size)]
+
+}
+
+fun fishFood (day : String) : String {
+    return when (day) {
+        "Monday" -> "flakes"
+        "Tuesday" -> "pellets"
+        "Wednesday" -> "redworms"
+        "Thursday" -> "granules"
+        "Friday" -> "mosquitoes"
+        "Saturday" -> "lettuce"
+        "Sunday" -> "plankton"
+        else -> "nothing"
+    }
+}
+
+fun feedTheFish() {
+    val day = randomDay()
+    val food = fishFood(day)
+    println ("Today is $day and the fish eat $food")
+    println("Change Water: ${shouldChangeWater(day)}")
+}
+
+fun swim(speed: String = "fast") {
+    println("swimming $speed")
+}
+fun shouldChangeWater (day: String, temperature: Int = 20, dirty: Int = 20): Boolean {
+    return when {
+        isTooHot(temperature) -> true
+        isDirty(dirty) -> true
+        isSunday(day) -> true
+        else -> false
+    }
+}
+
+fun isTooHot(temperature: Int) = temperature > 30
+fun isDirty(dirty: Int) = dirty > 30
+fun isSunday(day: String) = day =="Sunday"
+fun main (args: Array<String>) {
+    feedTheFish()
+    swim("turtle-like")
+
+}
